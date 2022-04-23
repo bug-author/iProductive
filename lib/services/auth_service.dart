a97db9d67ca4d46f7b09cee8b1ea7d3a81d108fd
shy-tan <iabdullahbintahir@gmail.com>
2022-04-23 05:49:26 +0500

    fix token/uid

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,23 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthClass {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  final GoogleSignIn _googleSignIn = GoogleSignIn(
-    scopes: [
-      'email',
-      'https://www.googleapis.com/auth/contacts.readonly',
-    ],
-  );
-
   final storage = const FlutterSecureStorage();
 
   Future<void> signOut({required BuildContext context}) async {
     try {
-      await _googleSignIn.signOut();
       await _auth.signOut();
       await storage.delete(key: "token");
     } catch (e) {
@@ -28,7 +19,7 @@ class AuthClass {
 
   void storeTokenAndData(UserCredential userCredential) async {
     await storage.write(
-        key: "token", value: userCredential.credential!.token.toString());
+        key: "token", value: userCredential.user!.uid.toString());
     await storage.write(
         key: "usercredential", value: userCredential.toString());
   }
